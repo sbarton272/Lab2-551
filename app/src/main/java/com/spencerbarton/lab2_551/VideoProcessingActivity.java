3a85b0071a50b8e511a11ee9cacf0922ed17622f
Spencer Barton <sbarton272@gmail.com>
2015-02-08 17:12:25 -0500

    OpenCV preview working again

--- app/src/main/java/com/spencerbarton/lab2_551/VideoProcessingActivity.java
+++ app/src/main/java/com/spencerbarton/lab2_551/VideoProcessingActivity.java
@@ -4,63 +4,76 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-// TODO install handler
-// TODO clean-up code
-// TODO make sure openCV loaded
-// TODO reduce conversions for openCV
-// TODO sizes not hard coded
-public class VideoProcessingActivity extends Activity {
 
+public class VideoProcessingActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
     private static final String TAG = "VideoProcessingActivity";
-    private static final int mPreviewSizeWidth = 640;
-    private static final int mPreviewSizeHeight = 480;
-    private CameraProcessing mCamProcessing;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    private void createPostOpenCv() {
-        setContentView(R.layout.activity_video_processing);
-        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.camera_preview);
-        mCamProcessing = new CameraProcessing(surfaceView.getHolder(), mPreviewSizeWidth, mPreviewSizeHeight);
-    }
-
-    /*=======================================================
-      State management
-      =======================================================*/
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
-    }
-
-    /*=======================================================
-      OpenCV
-      =======================================================*/
+    private CameraBridgeViewBase mOpenCvCameraView;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
-                case LoaderCallbackInterface.SUCCESS: {
+                case LoaderCallbackInterface.SUCCESS:
+                {
                     Log.i(TAG, "OpenCV loaded successfully");
-                    createPostOpenCv();
-                }
-                break;
-                default: {
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
                     super.onManagerConnected(status);
-                }
-                break;
+                } break;
             }
         }
     };
 
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_video_processing);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.video_surface);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
 }
